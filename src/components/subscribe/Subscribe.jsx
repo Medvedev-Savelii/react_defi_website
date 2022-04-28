1b902c3656961cf233721f00474e6f56007f877d
sava <savamedvedev@gmail.com>
2022-04-29 05:47:42 +0800

    four-commit

--- src/components/subscribe/Subscribe.jsx
+++ src/components/subscribe/Subscribe.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React from "react";
 import "./Subscribe.css";
 const Subscribe = () => {
   return (
-	<div>Subscribe</div>
-  )
-}
+    <div className="subscribe">
+      <div className="content">
+        <h2>Join Our DeFi Community</h2>
+        <form
+          action="https://getform.io/f/473f6bae-abaa-4a9a-8f09-44d68fe1409e"
+          method="POST"
+        >
+          <div className="form-container display-col">
+            <input type="email" name="email" placeholder="Enter your email" />
+            <button className="btn">Sign Up</button>
+          </div>
 
-export default Subscribe
+          <div className="form-container">
+            <input type="checkbox" />{" "}
+            <p>Yes, I agree to receive email communications from DeFi.</p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Subscribe;
